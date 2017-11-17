f45b3c59e44e60a16eb98475b01130fab6d0b470
kxy1107 <573240722@qq.com>
2017-11-17 14:03:22 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# myshopback
+# 我家小卖铺 一个基于Vue.js + Element 搭建的简单商城后台管理系统
 
-> A Vue.js project
+> <img width="216" height="384" src="https://github.com/kxy1107/XiaoXiaoStoreBack/raw/master/readmeSrc/ht1.png" alt="无法显示图片"/>
+><img width="216" height="384" src="https://github.com/kxy1107/XiaoXiaoStoreBack/raw/master/readmeSrc/ht2.png"  alt="无法显示图片"/>
+><img width="216" height="384" src="https://github.com/kxy1107/XiaoXiaoStoreBack/raw/master/readmeSrc/ht3.png"  alt="无法显示图片"/>
+
+[后台地址](http://jianyuejizhang.cn/)  
+测试方式，登录微信小程序搜索我家小卖铺，进入联系我，点击我也要上传商品，注册等待审核通过即可进入系统
 
 ## Build Setup
 
@@ -14,8 +19,4 @@ npm run dev
 # build for production with minification
 npm run build
 
-# build for production and view the bundle analyzer report
-npm run build --report
-```
 
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/page/Login.vue
+++ src/page/Login.vue
@@ -20,113 +20,116 @@
     </el-form>
 </template>
 <script>
-import extend from '@/extend.js'
+import extend from "@/extend.js";
 export default {
-    name: 'Login',
-    data() {
-        return {
-            userName:'',
-            userPassword:'',
-            checked: true,
-            logining: false,
-        }
+  name: "Login",
+  data() {
+    return {
+      userName: "",
+      userPassword: "",
+      checked: true,
+      logining: false
+    };
+  },
+  methods: {
+    //点击登陆
+    toLogin() {
+      let self = this;
+
+      if (self.checkInfo()) {
+        let url = extend.rootPath + "/login";
+        let data = {
+          Phone: self.userName,
+          Password: self.userPassword
+        };
+        this.$http.get(url, { params: data }).then(
+          function(successRes) {
+            if (successRes.data.Code == 1) {
+              self.isRemember();
+              let userInfo = JSON.stringify(successRes.data.UserInfo);
+              sessionStorage.setItem("userInfo", userInfo);
+              this.$router.push({ path: "/UserInfo" });
+            } else if (successRes.data.Code == 10) {
+              self.isRemember();
+              let userInfo = JSON.stringify(successRes.data.UserInfo);
+              sessionStorage.setItem("userInfo", userInfo);
+              this.$router.push({ path: "/AdminShoperManage" });
+            } else {
+              self.$message({
+                message: "账号或密码错误",
+                duration: 1500,
+                type: "error"
+              });
+            }
+          },
+          function(failRes) {}
+        );
+      }
     },
-    methods: {
-        //点击登陆
-        toLogin() {
-            let self = this;
-       
-            if(self.checkInfo()){
-                 let url = extend.rootPath + '/login';
-                    let data = {
-                        Phone: self.userName,
-                        Password:self.userPassword
-                    };
-                    this.$http.get(url, { params: data }).then(function (successRes) {
-                        if(successRes.data.Code == 1){
-                            self.isRemember();
-                            let userInfo =  JSON.stringify(successRes.data.UserInfo);
-                            sessionStorage.setItem("userInfo", userInfo);	
-                            this.$router.push({ path: '/UserInfo' });
-                        }else{
-                             self.isRemember();
-                             let userInfo =  JSON.stringify(successRes.data.UserInfo);
-                            sessionStorage.setItem("userInfo", userInfo);	
-                            this.$router.push({ path: '/AdminShoperManage' });
-                        }
-                       
-                    }, function (failRes) {
-
-                    });
-            }
-
-
-           
-        },
-        //校验账号密码
-        checkInfo(){
-          if(this.userName.replace(/(^s*)|(s*$)/g, "").length == 0){
-              this.$message({
-                message: '账号不能为空！',
-                duration:1500,
-                type: 'error'
-                });
-              return false;
-          }else if(this.userPassword.replace(/(^s*)|(s*$)/g, "").length == 0) {
-                this.$message({
-                message: '密码不能为空！',
-                 duration:1500,
-                type: 'error'
-                });
-              return false;
-          }
-          return true;
-        },
-
-        //是否记住密码
-        isRemember(){
-            if(this.checked){
-                extend.setCookie('UserName',this.userName,3);
-                extend.setCookie('UserPassword',this.userPassword,3);
-            }else{
-                extend.delCookie('UserName');
-                 extend.delCookie('UserPassword');
-            }
-        },
+    //校验账号密码
+    checkInfo() {
+      if (this.userName.replace(/(^s*)|(s*$)/g, "").length == 0) {
+        this.$message({
+          message: "账号不能为空！",
+          duration: 1500,
+          type: "error"
+        });
+        return false;
+      } else if (this.userPassword.replace(/(^s*)|(s*$)/g, "").length == 0) {
+        this.$message({
+          message: "密码不能为空！",
+          duration: 1500,
+          type: "error"
+        });
+        return false;
+      }
+      return true;
     },
 
-    //页面加载完
-    mounted(){
-        let userName = extend.getCookie('UserName') ;
-        let pwd = extend.getCookie('UserPassword') ;
-        if(userName != null && pwd != null){
-            this.userName = userName;
-            this.userPassword = pwd;
-        }
+    //是否记住密码
+    isRemember() {
+      if (this.checked) {
+        extend.setCookie("UserName", this.userName, 3);
+        extend.setCookie("UserPassword", this.userPassword, 3);
+      } else {
+        extend.delCookie("UserName");
+        extend.delCookie("UserPassword");
+      }
     }
-}
+  },
+
+  //页面加载完
+  mounted() {
+    let userName = extend.getCookie("UserName");
+    let pwd = extend.getCookie("UserPassword");
+    if (userName != null && pwd != null) {
+      this.userName = userName;
+      this.userPassword = pwd;
+    }
+  }
+};
 </script>
 <style>
 .login-content {
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    -moz-border-radius: 5px;
-    background-clip: padding-box;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-    width: 350px;
-    padding: 35px 35px 15px 35px;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  background-clip: padding-box;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
 }
 
 .login-content-title {
-    margin: 0px auto 40px auto;
-    text-align: center;
-    color: #505458;
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
 }
 
 .login-content-button {
-    width: 100%;
+  width: 100%;
 }
 </style>
 
